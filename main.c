80426625a067c7f3ee1d2ac5e16720b41b8d5529
X-EMF <136609943+x-emf@users.noreply.github.com>
2023-06-18 13:59:37 -0400

    This is so damn cool

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <time.h>
 
 #include "measure.h"
 
 int main(int argc, char** argv) {
-    uint64_t latency = measure_rdrand_latency();
-    printf("Latency in cycles: %ld\n", latency);
+    struct timespec delay;
+    delay.tv_sec = 0;
+    delay.tv_nsec = 50 * 1000 * 1000;
+    while (1) {
+        uint64_t latency = measure_rdrand_latency();
+        printf("Latency in cycles: %ld\n", latency);
+        nanosleep(&delay, &delay);
+    }
     return 0;
 }
