5f2eeb8df8c0094263589d28fe68b22e97248097
X-EMF <136609943+x-emf@users.noreply.github.com>
2023-07-07 13:50:53 -0400

    Improve timing code

--- measure.h
+++ measure.h
@@ -2,24 +2,28 @@
 #define __MEASURE_H__
 
 #include <stdint.h>
+#include <stdio.h>
 
 uint64_t measure_rdrand_latency() {
-    uint64_t cycles;
+    uint64_t sd, sa, ed, ea;
 
     asm volatile (
         "mfence\n\t"
         "lfence\n\t"
         "rdtscp\n\t"
-        "mov %%eax, %%edi\n\t"
+        "mov %%rdx, %0\n\t"
+        "mov %%rax, %1\n\t"
         "rdrand %%r8\n\t"
         "rdtscp\n\t"
-        "sub %%edi, %%eax"
-        : "=a"(cycles)
+        "mov %%rdx, %2\n\t"
+        "mov %%rax, %3\n\t"
+        : "=r"(sd), "=r"(sa), "=r"(ed), "=r"(ea)
         :
-        : "r8", "edi"
+        : "r8", "%rax", "%rbx", "%rcx", "%rdx"
     );
-
-    return cycles;
+    uint64_t start = (sd << 32) | sa;
+    uint64_t end   = (ed << 32) | ea;
+    return end - start;
 }
 
 #endif
